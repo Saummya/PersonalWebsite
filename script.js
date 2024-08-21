6faa2a9c4e069d49821c3352df93aa3496b99940
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2024-08-21 16:17:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Coder", "Web Developer", "Designer", "Learner"],
+        strings: ["Coder", "Data Scientist", "NLP Engineer", "Learner"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Coder", "Web Developer", "Designer", "Learner"],
+        strings: ["Coder", "Data Scientist", "NLP Engineer", "Learner"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
